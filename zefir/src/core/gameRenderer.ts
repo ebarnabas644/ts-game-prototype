99a292df1077ac8227182275c23f1bdcf15a1a84
ebarnabas <macskabal2000@gmail.com>
2023-06-29 17:10:41 +0200

    PIXI renderer added

--- zefir/src/core/gameRenderer.ts
+++ zefir/src/core/gameRenderer.ts
@@ -1,9 +1,39 @@
+import * as PIXI from 'pixi.js'
+import { type Player } from './models/player'
+
+let pixiApp: PIXI.Application;
+
 let pos = 50
 
-export function renderGame(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement){
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+export function setPIXI(app: PIXI.Application){
+    pixiApp = app
+}
 
-    ctx.fillStyle = "red";
-    ctx.fillRect(pos, 50, 100, 100)
-    pos++
+export function renderGame(){
+    let sprite = PIXI.Sprite.from('./plane.png')
+    pixiApp.stage.addChild(sprite)
+
+    let elapsed = 0.0;
+    // Tell our application's ticker to run a new callback every frame, passing
+    // in the amount of time that has passed since the last tick
+    pixiApp.ticker.add((delta) => {
+    // Add the time to our total elapsed time
+    elapsed += delta;
+    // Update the sprite's X position based on the cosine of our elapsed time.  We divide
+    // by 50 to slow the animation down a bit...
+    sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+  });
+}
+
+function createPlayer(player: Player){
+  let sprite = PIXI.Sprite.from('./plane.png')
+  pixiApp.stage.addChild(sprite)
+}
+
+function drawPlayer(player: Player){
+  let sprite = PIXI.Sprite.from('./plane.png')
+  sprite.x = player.x
+  sprite.y = player.y
+  sprite.height = player.height
+  sprite.width = player.width
 }
