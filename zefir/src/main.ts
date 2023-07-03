7824109d0bd78f8a073ca7dc5dc373286802d76a
ebarnabas <macskabal2000@gmail.com>
2023-07-03 23:01:02 +0200

    Store created event added

--- zefir/src/main.ts
+++ zefir/src/main.ts
@@ -1,14 +1,26 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { VueElement, createApp } from 'vue'
+import { createPinia, storeToRefs } from 'pinia'
+import { piniaPlugin } from './piniaPlugin'
+import { usePlayerStatStore } from './stores/counter'
 
 import App from './App.vue'
 import router from './router'
 
+export const pinia = createPinia()
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
+app.use(piniaPlugin)
 app.use(router)
 
 app.mount('#app')
+function emitCustomEvent(eventName: string, payload: any) {
+    const event = new CustomEvent(eventName, { detail: payload });
+    document.dispatchEvent(event);
+  }
+
+const testStore = usePlayerStatStore()
+emitCustomEvent('storeReady', testStore)
+window.myStore = testStore
