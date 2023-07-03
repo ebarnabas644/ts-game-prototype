8d4d77ffed75ce1b515da1e7d131500bf3ffe5a7
ebarnabas <macskabal2000@gmail.com>
2023-07-03 22:47:25 +0200

    Testing pinia

--- zefir/src/stores/counter.ts
+++ zefir/src/stores/counter.ts
@@ -1,5 +1,6 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { playerHealth } from '@/core/entity/entity'
 
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
@@ -9,4 +10,12 @@ export const useCounterStore = defineStore('counter', () => {
   }
 
   return { count, doubleCount, increment }
+})
+
+export const usePlayerStatStore = defineStore('playerStat', () => {
+  const health = ref([{health: {max: 100, current: 50}, mana: 10}, {health: {max: 100, current: 10}, mana: 10}, {health: {max: 120, current: 20}, mana: 10}])
+  function decrease(value: number){
+    health.value[0].health.current = value
+  }
+  return { health, decrease }
 })
