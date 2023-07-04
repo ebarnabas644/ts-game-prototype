ea9398f1c9eccb93a6f24a0248c06633ac7e9f88
ebarnabas <macskabal2000@gmail.com>
2023-07-04 17:26:01 +0200

    Cleaning up app initialization

--- zefir/src/main.ts
+++ zefir/src/main.ts
@@ -2,25 +2,20 @@ import './assets/main.css'
 
 import { VueElement, createApp } from 'vue'
 import { createPinia, storeToRefs } from 'pinia'
-import { piniaPlugin } from './piniaPlugin'
-import { usePlayerStatStore } from './stores/counter'
+import { usePlayerStatStore } from './stores/entity'
+import { setStore } from './core/gameState'
 
 import App from './App.vue'
 import router from './router'
+import { initGame } from './core/gameMain'
 
 export const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia)
-app.use(piniaPlugin)
 app.use(router)
+const testStore = usePlayerStatStore()
+setStore(testStore)
+initGame()
 
 app.mount('#app')
-function emitCustomEvent(eventName: string, payload: any) {
-    const event = new CustomEvent(eventName, { detail: payload });
-    document.dispatchEvent(event);
-  }
-
-const testStore = usePlayerStatStore()
-emitCustomEvent('storeReady', testStore)
-window.myStore = testStore
