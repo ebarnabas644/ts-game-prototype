bcee6057f4fe10f0d2f4737bf809709b281ea6e3
ebarnabas <macskabal2000@gmail.com>
2023-07-05 17:09:55 +0200

    Entity renderer added

--- zefir/src/core/gameRenderer.ts
+++ zefir/src/core/gameRenderer.ts
@@ -2,11 +2,17 @@ import * as PIXI from 'pixi.js'
 import { type Player } from './models/player'
 import { Entity } from './entity/entity'
 
+type RenderDictionary = {[key: number]: PIXI.Sprite}
+
 export class RendererSystemComponent
 {
   public pixiApp: PIXI.Application
+  public renderDictionary: RenderDictionary
+  private maintanceQueue: Set<number>
   constructor(pixi: PIXI.Application){
     this.pixiApp = pixi
+    this.renderDictionary = {}
+    this.maintanceQueue = new Set()
   }
 
   public Start(){
@@ -23,6 +29,45 @@ export class RendererSystemComponent
     // by 50 to slow the animation down a bit...
     sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
   });
+  }
+
+  public update(entities: EntityDTODictionary){
+    for (const key in entities) {
+      for (const entityKey in entities[key]) {
+        if(!this.isAlreadyCreated(entities[key][entityKey].id)){
+          this.renderDictionary[entities[key][entityKey].id] = PIXI.Sprite.from('./src/core/sprites/'+entities[key][entityKey].sprite)
+          this.pixiApp.stage.addChild(this.renderDictionary[entities[key][entityKey].id])
+        }
+        this.updatePosition(entities[key][entityKey])
+        this.maintanceQueue.delete(entities[key][entityKey].id)
+      }
+    }
+    this.removeDestroyedEntities()
+    this.copyMaintanceQueue()
+    
+  }
+
+  private isAlreadyCreated(id: number){
+    return this.renderDictionary[id]
+  }
+
+  private updatePosition(entityDTO: EntityDTO){
+    this.renderDictionary[entityDTO.id].x = entityDTO.position.x
+    this.renderDictionary[entityDTO.id].y = entityDTO.position.y
+  }
+
+  private removeDestroyedEntities(){
+    this.maintanceQueue.forEach(id => {
+      this.pixiApp.stage.removeChild(this.renderDictionary[Number(id)])
+      delete this.renderDictionary[Number(id)]
+    });
+  }
+
+  private copyMaintanceQueue(){
+    this.maintanceQueue.clear()
+    for (const key in this.renderDictionary) {
+      this.maintanceQueue.add(Number(key))
+    }
   }
 }
 
