3b5b05313bc64be75279f78c6757ee150245c0a1
ebarnabas <macskabal2000@gmail.com>
2023-07-05 17:09:00 +0200

    Basic input handler added

--- zefir/src/core/gameInput.ts
+++ zefir/src/core/gameInput.ts
@@ -1,6 +1,39 @@
+import { emitCustomEvent } from "./utilities/customEventEmitter"
+
 export class InputSystemComponent{
-    private pushedButtons: string[]
+    private pushedButtons: Set<string>
     constructor(){
-        this.pushedButtons = []
+        this.pushedButtons = new Set()
+
+        document.addEventListener('keydown', (event) => {
+            if(event.key == "ArrowLeft"){
+                this.pushedButtons.add('leftMoveCommand')
+            }
+            if(event.key == "ArrowRight"){
+                this.pushedButtons.add('rightMoveCommand')
+            }
+            if(event.key == "ArrowUp"){
+                this.pushedButtons.add('upMoveCommand')
+            }
+            if(event.key == "ArrowDown"){
+                this.pushedButtons.add('downMoveCommand')
+            }
+            emitCustomEvent('playerInput', this.pushedButtons)
+          })
+        document.addEventListener('keyup', (event) => {
+        if(event.key == "ArrowLeft"){
+            this.pushedButtons.delete('leftMoveCommand')
+        }
+        if(event.key == "ArrowRight"){
+            this.pushedButtons.delete('rightMoveCommand')
+        }
+        if(event.key == "ArrowUp"){
+            this.pushedButtons.delete('upMoveCommand')
+        }
+        if(event.key == "ArrowDown"){
+            this.pushedButtons.delete('downMoveCommand')
+        }
+        emitCustomEvent('playerInput', this.pushedButtons)
+        })
     }
 }
