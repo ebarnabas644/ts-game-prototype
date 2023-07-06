d1c78cf5d3115d2f8c1192bf82ee80fb221f4127
ebarnabas <macskabal2000@gmail.com>
2023-07-06 16:53:56 +0200

    Reducing the number of events sent to the server & wasd controll

--- zefir/src/core/gameInput.ts
+++ zefir/src/core/gameInput.ts
@@ -2,38 +2,54 @@ import { emitCustomEvent } from "./utilities/customEventEmitter"
 
 export class InputSystemComponent{
     private pushedButtons: Set<string>
+    private previous: Set<string>
     constructor(){
         this.pushedButtons = new Set()
+        this.previous = new Set()
 
         document.addEventListener('keydown', (event) => {
-            if(event.key == "ArrowLeft"){
+            this.previous = new Set(this.pushedButtons)
+            if(event.key == "ArrowLeft" || event.key == "a"){
                 this.pushedButtons.add('leftMoveCommand')
             }
-            if(event.key == "ArrowRight"){
+            if(event.key == "ArrowRight" || event.key == "d"){
                 this.pushedButtons.add('rightMoveCommand')
             }
-            if(event.key == "ArrowUp"){
+            if(event.key == "ArrowUp" || event.key == "w"){
                 this.pushedButtons.add('upMoveCommand')
             }
-            if(event.key == "ArrowDown"){
+            if(event.key == "ArrowDown" || event.key == "s"){
                 this.pushedButtons.add('downMoveCommand')
             }
-            emitCustomEvent('playerInput', this.pushedButtons)
+
+            if(!this.previouslyPressedEqualsCurrentlyPressed()){
+                emitCustomEvent('playerInput', this.pushedButtons)
+            }
           })
         document.addEventListener('keyup', (event) => {
-        if(event.key == "ArrowLeft"){
+        if(event.key == "ArrowLeft" || event.key == "a"){
             this.pushedButtons.delete('leftMoveCommand')
         }
-        if(event.key == "ArrowRight"){
+        if(event.key == "ArrowRight" || event.key == "d"){
             this.pushedButtons.delete('rightMoveCommand')
         }
-        if(event.key == "ArrowUp"){
+        if(event.key == "ArrowUp" || event.key == "w"){
             this.pushedButtons.delete('upMoveCommand')
         }
-        if(event.key == "ArrowDown"){
+        if(event.key == "ArrowDown" || event.key == "s"){
             this.pushedButtons.delete('downMoveCommand')
         }
         emitCustomEvent('playerInput', this.pushedButtons)
         })
+    }
+
+    private previouslyPressedEqualsCurrentlyPressed(){
+        for (const button of this.pushedButtons) {
+            if(!this.previous.has(button)){
+                return false
+            }
+        }
+
+        return true
     }
 }
