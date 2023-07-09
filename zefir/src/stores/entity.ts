9981d324be61aa7632d59929e95301efa6cc1742
ebarnabas <macskabal2000@gmail.com>
2023-07-09 23:08:54 +0200

    Player controlled entity reference added

--- zefir/src/stores/entity.ts
+++ zefir/src/stores/entity.ts
@@ -1,10 +1,14 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import { Entity } from '@/core/entity/entity'
+import { networkSystemComponent } from '@/core/gameMain'
 
 export const usePlayerStatStore = defineStore('playerStat', () => {
   const isLoading = ref(true)
-  const entities = ref<EntityDTODictionary>()
+  const controlledEntity = computed(() => {
+    return entities.value['players'].find(entity => entity.tags['controlledby'] == networkSystemComponent.getConnectionId())
+  })
+  const entities = ref<EntityDTODictionary>({})
   const players = ref<{[key: string]: Entity }>({})
-  return { players, entities, isLoading }
+  return { players, entities, isLoading, controlledEntity }
 })
