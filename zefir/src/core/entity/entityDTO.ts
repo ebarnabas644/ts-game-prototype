fc92e0c070d98ad4be0f8569b812e8bec85eefac
ebarnabas <macskabal2000@gmail.com>
2023-07-09 22:44:18 +0200

    Tags added

--- zefir/src/core/entity/entityDTO.ts
+++ zefir/src/core/entity/entityDTO.ts
@@ -2,7 +2,8 @@ type EntityDTO = {
     id: number,
     health: number,
     position: { x: number, y: number },
-    sprite: string
+    sprite: string,
+    tags: {[key: string]: any}
 }
 
 type EntityDTODictionary = { [key: string]: EntityDTO[]}
