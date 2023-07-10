d6bac1436c11f2c90ce1be794dd30002321bb718
ebarnabas <macskabal2000@gmail.com>
2023-07-10 18:58:58 +0200

    Basic coordinate only minimap added

---
+++ zefir/src/components/Minimap.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Modal from './Modal.vue';
+import { usePlayerStatStore } from '@/stores/entity';
+
+const playerStat = usePlayerStatStore()
+</script>
+
+<template>
+    <Modal :default-x="0.8" :default-y="0.1" :modalwidth="100" :modalheight="100">
+        <h2>X:{{ playerStat.controlledEntity?.position.x }}</h2>
+        <h2>Y:{{ playerStat.controlledEntity?.position.y }}</h2>
+    </Modal>
+</template>
