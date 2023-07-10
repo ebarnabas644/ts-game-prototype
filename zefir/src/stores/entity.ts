630c53872839188d456336a4c834b1a1e6a6cb07
ebarnabas <macskabal2000@gmail.com>
2023-07-10 19:01:34 +0200

    Replacing types

--- zefir/src/stores/entity.ts
+++ zefir/src/stores/entity.ts
@@ -1,14 +1,15 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-import { Entity } from '@/core/entity/entity'
+import type { EntityDTODictionary } from '@/core/entity/entityDTO'
 import { networkSystemComponent } from '@/core/gameMain'
+import type { SimpleEntity } from '@/core/entity/simpleEntity'
 
 export const usePlayerStatStore = defineStore('playerStat', () => {
   const isLoading = ref(true)
   const controlledEntity = computed(() => {
-    return entities.value['players'].find(entity => entity.tags['controlledby'] == networkSystemComponent.getConnectionId())
+    return entities.value.find(entity => entity.tags['controlledby'] == networkSystemComponent.getConnectionId())
   })
-  const entities = ref<EntityDTODictionary>({})
-  const players = ref<{[key: string]: Entity }>({})
+  const entities = ref<EntityDTODictionary>([])
+  const players = ref<{[key: string]: SimpleEntity }>({})
   return { players, entities, isLoading, controlledEntity }
 })
