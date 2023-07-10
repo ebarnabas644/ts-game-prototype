6d4044a08d913c78ae53018afc82c87e0686e0f6
ebarnabas <macskabal2000@gmail.com>
2023-07-10 16:03:49 +0200

    Converter methods added

--- zefir/src/core/entity/entityDTO.ts
+++ zefir/src/core/entity/entityDTO.ts
@@ -1,9 +1,26 @@
-type EntityDTO = {
+import { SimpleEntity, type SimpleEntityDictionary } from "./simpleEntity"
+
+export type EntityDTO = {
     id: number,
+    name: string
     health: number,
     position: { x: number, y: number },
     sprite: string,
     tags: {[key: string]: any}
 }
 
-type EntityDTODictionary = { [key: string]: EntityDTO[]}
+export type EntityDTODictionary = EntityDTO[]
+
+export function convertEntityDTOtoSimpleEntity(entityDTO: EntityDTO){
+    return new SimpleEntity(entityDTO.id, entityDTO.name, entityDTO.position, './src/core/sprites/'+entityDTO.sprite, entityDTO.health, entityDTO.tags)
+}
+
+export function convertEntityDTODictionaryToSimpleEntityDictionary(entityDTODictionary: EntityDTODictionary){
+    const simpleEntityDictionary: SimpleEntityDictionary = []
+    for (let index = 0; index < entityDTODictionary.length; index++) {
+        const element = convertEntityDTOtoSimpleEntity(entityDTODictionary[index]);
+        simpleEntityDictionary.push(element)
+    }
+
+    return simpleEntityDictionary
+}
