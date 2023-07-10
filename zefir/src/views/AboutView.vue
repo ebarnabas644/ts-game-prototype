a31594a0e90e3de75c03ab52c0e105569f8780b1
ebarnabas <macskabal2000@gmail.com>
2023-07-10 19:01:49 +0200

    Basic login page added

--- zefir/src/views/AboutView.vue
+++ zefir/src/views/AboutView.vue
@@ -1,6 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { networkSystemComponent } from '@/core/gameMain';
+import { useRouter } from 'vue-router';
+import { emitCustomEvent } from '@/core/utilities/customEventEmitter';
+
+const nameInput = ref("")
+const router = useRouter()
+
+function sendLoginName(name: string){
+  networkSystemComponent.sendPlayerName(name)
+  router.push('/')
+  emitCustomEvent('startGame', '')
+}
+
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <br>
+    Enter your name:
+    <input class="text-black" type="text" v-model="nameInput">
+    <br>
+    <button class="border-white" @click="sendLoginName(nameInput)">Play!</button>
   </div>
 </template>
 
