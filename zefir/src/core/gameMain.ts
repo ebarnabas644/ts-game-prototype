c2727787b1e3fe90bf43754119587e68c77da54c
ebarnabas <macskabal2000@gmail.com>
2023-07-10 16:04:48 +0200

    Refactoring & camera follow test

--- zefir/src/core/gameMain.ts
+++ zefir/src/core/gameMain.ts
@@ -1,36 +1,47 @@
 import { NetworkSystemComponent } from "./gameNetwork";
 import { RendererSystemComponent } from "./gameRenderer"
 import * as PIXI from 'pixi.js'
-import { Entity } from "./entity/entity";
-import { HealthComponent } from "./entity/Components/healthComponent";
-import { PlayerControllerComponent } from "./entity/Components/playerControllerComponent";
-import { PositionComponent } from "./entity/Components/positionComponent";
-import { SpriteComponent } from "./entity/Components/spriteComponent";
 import { emitCustomEvent } from './utilities/customEventEmitter'
 import { store } from "./gameState";
 import { InputSystemComponent } from "./gameInput";
 import { GameCamera } from "./gameCamera"
+import { convertEntityDTODictionaryToSimpleEntityDictionary } from "./entity/entityDTO";
+import { GameState } from "./gameState";
 
 export let networkSystemComponent: NetworkSystemComponent
 export let rendererSystemComponent: RendererSystemComponent
+export let gameState: GameState
 let gameCamera: GameCamera
 let inputSystemComponent: InputSystemComponent
+let counter = 0
 
 export function initGame(){
+    gameState = new GameState()
+    counter = 0
     inputSystemComponent = new InputSystemComponent()
     networkSystemComponent = new NetworkSystemComponent()
     networkSystemComponent.initConnection()
     emitCustomEvent('engineReady', '')
     document.addEventListener('stateReceived', (event: any) => {
-      store.entities = event.detail
-      rendererSystemComponent.update(store.entities)
+      gameState.updateGameState(event.detail)
+      rendererSystemComponent.update(gameState.gameState)
+      if(counter == 0){
+        const findControlled = gameState.gameState.find(entity => entity.tags["controlledby"] == networkSystemComponent.getConnectionId())
+        console.log(findControlled)
+        if(findControlled){
+          gameCamera.setFollow(findControlled)
+          counter++
+          console.log("camera set")
+        }
+        
+      }
     })
+    store.isLoading = false
 }
 
 export function setRenderer(pixiApp: PIXI.Application){
     gameCamera = new GameCamera(pixiApp)
     rendererSystemComponent = new RendererSystemComponent(pixiApp, gameCamera)
-    initTestPlayers()
     rendererSystemComponent.Start()
     /*
     rendererSystemComponent.pixiApp.ticker.add((delta) => {
@@ -39,19 +50,4 @@ export function setRenderer(pixiApp: PIXI.Application){
         player.updateComponents(delta)
       }
     })*/
-}
-
-function initTestPlayers(){
-  const player2 = new Entity('player2')
-  const player = new Entity('player')
-  player.addComponent('position', new PositionComponent(100,50))
-  player.addComponent('health', new HealthComponent(50))
-  player.addComponent('playerController', new PlayerControllerComponent())
-  //player.addComponent('sprite', new SpriteComponent('./src/core/sprites/player.png'))
-  const component = player.getComponent('sprite') as SpriteComponent
-  const spriteComp = player.getComponent('sprite') as SpriteComponent
-  player2.addComponent('position', new PositionComponent(100,50))
-  player2.addComponent('health', new HealthComponent(50))
-  store.isLoading = false
-  //console.log(store)
 }
