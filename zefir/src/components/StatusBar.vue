6874422cfd5215af504b9e430a3b0dfdf9ae07f0
ebarnabas <macskabal2000@gmail.com>
2023-07-10 19:00:01 +0200

    Basic status bar added

---
+++ zefir/src/components/StatusBar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Modal from './Modal.vue';
+import { usePlayerStatStore } from '@/stores/entity';
+
+const playerStat = usePlayerStatStore()
+</script>
+
+<template>
+    <Modal :default-x="0.1" :default-y="0.1" :modalwidth="100" :modalheight="100">
+        <h2>Health:{{ playerStat.controlledEntity?.health }}</h2>
+    </Modal>
+</template>
