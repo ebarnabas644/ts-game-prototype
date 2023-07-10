9bb0707c7c74432de8ffc88be5530b82dc996299
ebarnabas <macskabal2000@gmail.com>
2023-07-10 16:05:28 +0200

    Update method added

--- zefir/src/core/gameState.ts
+++ zefir/src/core/gameState.ts
@@ -1,5 +1,25 @@
+import { convertEntityDTODictionaryToSimpleEntityDictionary, type EntityDTODictionary } from "./entity/entityDTO";
+import type { SimpleEntityDictionary } from "./entity/simpleEntity";
+
 export let store;
 
 export function setStore(storeToSet: any){
     store = storeToSet
+}
+
+export function setGameState(state: any){
+    
+}
+
+export class GameState{
+    public gameState: SimpleEntityDictionary
+
+    constructor(){
+        this.gameState = []
+    }
+
+    updateGameState(state: EntityDTODictionary){
+        state.entities = state
+        this.gameState = convertEntityDTODictionaryToSimpleEntityDictionary(state)
+    }
 }
