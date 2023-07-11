861fd9e8f17a9ab9e6877183e54a21f4c2683b1e
ebarnabas <macskabal2000@gmail.com>
2023-07-11 14:14:16 +0200

    Game view redirects player to login site if not logged in yet

--- zefir/src/views/GameView.vue
+++ zefir/src/views/GameView.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
 import Canvas from '@/components/Canvas.vue';
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import { usePlayerStatStore } from '@/stores/entity';
+
+const router = useRouter()
+const playerStatStore = usePlayerStatStore()
+
+onMounted(() => {
+  if(!playerStatStore.isLoggedIn){
+    router.push('/')
+  }
+})
 </script>
 
 <template>
