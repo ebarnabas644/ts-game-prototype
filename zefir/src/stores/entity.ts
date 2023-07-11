e40803ce4eca6f9edbe962da4378ee56544cedf7
ebarnabas <macskabal2000@gmail.com>
2023-07-11 14:13:28 +0200

    Logged in state added

--- zefir/src/stores/entity.ts
+++ zefir/src/stores/entity.ts
@@ -6,10 +6,11 @@ import type { SimpleEntity } from '@/core/entity/simpleEntity'
 
 export const usePlayerStatStore = defineStore('playerStat', () => {
   const isLoading = ref(true)
+  const isLoggedIn = ref(false)
   const controlledEntity = computed(() => {
     return entities.value.find(entity => entity.tags['controlledby'] == networkSystemComponent.getConnectionId())
   })
   const entities = ref<EntityDTODictionary>([])
   const players = ref<{[key: string]: SimpleEntity }>({})
-  return { players, entities, isLoading, controlledEntity }
+  return { players, entities, isLoading, controlledEntity, isLoggedIn }
 })
