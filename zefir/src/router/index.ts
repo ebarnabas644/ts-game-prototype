c6efb19fe38d99e7fc50eefb9975ecab9e13d71b
ebarnabas <macskabal2000@gmail.com>
2023-07-11 14:13:18 +0200

    Replacing home route with login page

--- zefir/src/router/index.ts
+++ zefir/src/router/index.ts
@@ -5,13 +5,13 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: '/game',
+      name: 'Game',
       component: GameView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/',
+      name: 'Login',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
