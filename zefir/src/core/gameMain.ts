d1473c4cf1507b7f58d7cf2ac558265f1c7b31c0
ebarnabas <macskabal2000@gmail.com>
2023-07-12 17:07:26 +0200

    Clearing up code

--- zefir/src/core/gameMain.ts
+++ zefir/src/core/gameMain.ts
@@ -10,31 +10,25 @@ import { GameState } from "./gameState";
 
 export let networkSystemComponent: NetworkSystemComponent
 export let rendererSystemComponent: RendererSystemComponent
+export let inputSystemComponent: InputSystemComponent
 export let gameState: GameState
 let gameCamera: GameCamera
-export let inputSystemComponent: InputSystemComponent
-let counter = 0
+
 
 export function initGame(){
   networkSystemComponent = new NetworkSystemComponent()
   networkSystemComponent.initConnection()
     document.addEventListener('playerReceived', (event) => {
       gameState = new GameState()
-      counter = 0
       inputSystemComponent = new InputSystemComponent()
-      emitCustomEvent('engineReady', '')
       rendererSystemComponent.Start(gameState)
       document.addEventListener('stateReceived', (event: any) => {
         gameState.updateGameState(event.detail)
         // TODO: Hell no pls fix this future me
-        if(counter == 0){
-          const findControlled = gameState.gameState.find(entity => entity.tags["controlledby"] == networkSystemComponent.getConnectionId())
+        const findControlled = gameState.gameState.find(entity => entity.tags["controlledby"] == networkSystemComponent.getConnectionId())
           if(findControlled){
             gameCamera.setFollow(findControlled)
-            //counter++
           }
-          
-        }
       })
       store.isLoading = false
     })
@@ -43,11 +37,4 @@ export function initGame(){
 export function setRenderer(pixiApp: PIXI.Application){
     gameCamera = new GameCamera(pixiApp)
     rendererSystemComponent = new RendererSystemComponent(pixiApp, gameCamera)
-    /*
-    rendererSystemComponent.pixiApp.ticker.add((delta) => {
-      for (const key in store.players){
-        const player = store.players[key]
-        player.updateComponents(delta)
-      }
-    })*/
 }
