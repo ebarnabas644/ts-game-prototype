8bebf112f3dfa95519a41a43a2dd6f05189d35c7
ebarnabas <macskabal2000@gmail.com>
2023-07-12 17:08:40 +0200

    Clearing up code

--- zefir/src/core/gameRenderer.ts
+++ zefir/src/core/gameRenderer.ts
@@ -15,67 +15,22 @@ export class RendererSystemComponent
   private gameCamera: GameCamera
   public renderDictionary: RenderDictionary
   private maintanceQueue: Set<number>
-  private lastPlayerX: number
-  private lastPlayerY: number
   constructor(pixi: PIXI.Application, gameCamera: GameCamera){
     this.pixiApp = pixi
     this.gameCamera = gameCamera
     this.renderDictionary = {}
     this.maintanceQueue = new Set()
-    this.lastPlayerX = 0
-    this.lastPlayerY = 0
     this.renderMap()
   }
 
   public Start(gameState: GameState){
-    let sprite = PIXI.Sprite.from('./plane.png')
-    //this.pixiApp.stage.addChild(sprite)
-    let elapsed = 0.0;
-    // Tell our application's ticker to run a new callback every frame, passing
-    // in the amount of time that has passed since the last tick
     this.pixiApp.ticker.add((delta) => {
+      
     this.update(gameState.gameState)
-    // Add the time to our total elapsed time
-    elapsed += delta;
-    //console.log(PIXI.Ticker.shared.deltaMS)
-    // Update the sprite's X position based on the cosine of our elapsed time.  We divide
-    // by 50 to slow the animation down a bit...
-    sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
   });
   }
 
   public update(entities: SimpleEntityDictionary){
-    /*if(entities[0]){
-      let interpolated: Vec2 = new Vec2()
-      let entity = entities[0]
-      v.mixN(interpolated, [this.lastPlayerX, this.lastPlayerY], [entity.position.x, entity.position.y], 0.5)
-      this.lastPlayerX = interpolated.x
-      this.lastPlayerY = interpolated.y
-      if(entities[0].position.x != this.lastPlayerX || entities[0].position.y != this.lastPlayerY){
-        let interpolated: Vec2 = new Vec2()
-        let entity = entities[0]
-        if(entity){
-          v.mixN(interpolated, [this.lastPlayerX, this.lastPlayerY], [entity.position.x, entity.position.y], 0.5)
-          entities[0].position.x = interpolated.x
-          entities[0].position.y = interpolated.y
-          this.lastPlayerX = interpolated.x
-          this.lastPlayerY = interpolated.y
-        }
-        //console.log('New position arrived')
-      }
-      else{
-        let interpolated: Vec2 = new Vec2()
-        let entity = entities[0]
-        if(entity){
-          v.mixN(interpolated, [this.lastPlayerX, this.lastPlayerY], [entity.position.x, entity.position.y], 0.5)
-          entities[0].position.x = interpolated.x
-          entities[0].position.y = interpolated.y
-          this.lastPlayerX = interpolated.x
-          this.lastPlayerY = interpolated.y
-        }
-        //console.log('Position unchanged')
-      }
-    }*/
     for (let index = 0; index < entities.length; index++) {
       const entity = entities[index];
       if(!this.isAlreadyCreated(entity.id)){
@@ -86,7 +41,6 @@ export class RendererSystemComponent
     }
     this.removeDestroyedEntities()
     this.copyMaintanceQueue()
-    //console.log(this.pixiApp)
   }
 
   private isAlreadyCreated(id: number){
