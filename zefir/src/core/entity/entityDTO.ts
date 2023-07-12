0973a4375712f2ef55166e6e58f8d6e20e315dc9
ebarnabas <macskabal2000@gmail.com>
2023-07-12 17:06:24 +0200

    Animation state added

--- zefir/src/core/entity/entityDTO.ts
+++ zefir/src/core/entity/entityDTO.ts
@@ -6,13 +6,14 @@ export type EntityDTO = {
     health: number,
     position: { x: number, y: number },
     sprite: string,
+    state: string,
     tags: {[key: string]: any}
 }
 
 export type EntityDTODictionary = EntityDTO[]
 
 export function convertEntityDTOtoSimpleEntity(entityDTO: EntityDTO){
-    return new SimpleEntity(entityDTO.id, entityDTO.name, entityDTO.position, './src/core/sprites/'+entityDTO.sprite, entityDTO.health, entityDTO.tags)
+    return new SimpleEntity(entityDTO.id, entityDTO.name, entityDTO.position, './src/core/sprites/'+entityDTO.sprite, entityDTO.health, entityDTO.tags, entityDTO.state)
 }
 
 export function convertEntityDTODictionaryToSimpleEntityDictionary(entityDTODictionary: EntityDTODictionary){
