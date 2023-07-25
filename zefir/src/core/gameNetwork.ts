0c8fa3a372e83bda01ba22bc76ba14040d49f7fb
ebarnabas <macskabal2000@gmail.com>
2023-07-25 10:49:33 +0200

    Transmitting player name

--- zefir/src/core/gameNetwork.ts
+++ zefir/src/core/gameNetwork.ts
@@ -24,8 +24,8 @@ export class NetworkSystemComponent{
         emitCustomEvent('stateReceived', entities)
       })
 
-      this.socket.on('playerCreated', () => {
-        emitCustomEvent('playerReceived', '')
+      this.socket.on('playerCreated', (name: string) => {
+        emitCustomEvent('playerReceived', name)
       })
       this.socket.on('disconnect', (reason: any) => {
         console.log('Connection lost because '+reason)
