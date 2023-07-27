77c1eeafdc5eb01d666b4f185a4f61680b83f755
ebarnabas <macskabal2000@gmail.com>
2023-07-27 17:07:22 +0200

    Formatting and address change

--- zefir/src/core/gameNetwork.ts
+++ zefir/src/core/gameNetwork.ts
@@ -3,59 +3,59 @@ import { emitCustomEvent } from './utilities/customEventEmitter'
 import type { EntityDTO, EntityDTODictionary } from './entity/entityDTO'
 
 export class NetworkSystemComponent {
-  private socket: Socket
+        private socket: Socket
 
-  constructor() {
-    this.socket = io('http://localhost:3000', {
-      transports: ['websocket'],
-      upgrade: false
-    })
-  }
+        constructor() {
+                this.socket = io('https://zefir.server.iedre.dev', {
+                        transports: ['websocket'],
+                        upgrade: false
+                })
+        }
 
-  public initConnection() {
-    this.socket.on('connect', () => {
-      console.time()
-      console.log('Connected to the server')
-    })
-    this.socket.on('response', (message: string) => {
-      emitCustomEvent('chatMessage', message)
-    })
-    this.socket.on('state', (entities: EntityDTODictionary) => {
-      emitCustomEvent('stateReceived', entities)
-    })
+        public initConnection() {
+                this.socket.on('connect', () => {
+                        console.time()
+                        console.log('Connected to the server')
+                })
+                this.socket.on('response', (message: string) => {
+                        emitCustomEvent('chatMessage', message)
+                })
+                this.socket.on('state', (entities: EntityDTODictionary) => {
+                        emitCustomEvent('stateReceived', entities)
+                })
 
-    this.socket.on('playerCreated', (name: string) => {
-      emitCustomEvent('playerReceived', name)
-    })
-    this.socket.on('chat-playerJoined', (name: string) => {
-      emitCustomEvent('chat-playerJoined', name)
-    })
-    this.socket.on('disconnect', (reason: any) => {
-      console.log('Connection lost because ' + reason)
-      console.timeEnd()
-    })
-  }
+                this.socket.on('playerCreated', (name: string) => {
+                        emitCustomEvent('playerReceived', name)
+                })
+                this.socket.on('chat-playerJoined', (name: string) => {
+                        emitCustomEvent('chat-playerJoined', name)
+                })
+                this.socket.on('disconnect', (reason: any) => {
+                        console.log('Connection lost because ' + reason)
+                        console.timeEnd()
+                })
+        }
 
-  public sendConnectionRequest(name: string) {
-    this.socket.emit('newPlayer', name)
-    this.registerPlayerCommandEvents()
-  }
+        public sendConnectionRequest(name: string) {
+                this.socket.emit('newPlayer', name)
+                this.registerPlayerCommandEvents()
+        }
 
-  public getConnectionId(): string {
-    return this.socket.id
-  }
+        public getConnectionId(): string {
+                return this.socket.id
+        }
 
-  public sendMessage(event: string, message: string) {
-    this.socket.emit(event, message)
-  }
+        public sendMessage(event: string, message: string) {
+                this.socket.emit(event, message)
+        }
 
-  private registerPlayerCommandEvents() {
-    document.addEventListener('playerInput', (event: any) => {
-      const commands: string[] = []
-      event.detail.forEach((command: any) => {
-        commands.push(command)
-      })
-      this.socket.emit('playerCommand', commands)
-    })
-  }
+        private registerPlayerCommandEvents() {
+                document.addEventListener('playerInput', (event: any) => {
+                        const commands: string[] = []
+                        event.detail.forEach((command: any) => {
+                                commands.push(command)
+                        })
+                        this.socket.emit('playerCommand', commands)
+                })
+        }
 }
