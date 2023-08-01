a2e61041b0ee7b710715d84949770eb61dd51c32
ebarnabas <macskabal2000@gmail.com>
2023-08-01 16:49:51 +0200

    Prettier

--- zefir/src/core/data/spriteLibrary.ts
+++ zefir/src/core/data/spriteLibrary.ts
@@ -1,47 +1,45 @@
 import type { SpritesheetBuilder } from '../utilities/spriteSheetBuilder'
 import { type Sheet } from '../types/sheet'
 
-export class SpriteLibrary{
-    private basePath: string
-    private textureCollection: {[key: string]: { [key: string]: Sheet }}
-    private spritesheetBuilder: SpritesheetBuilder
+export class SpriteLibrary {
+        private basePath: string
+        private textureCollection: { [key: string]: { [key: string]: Sheet } }
+        private spritesheetBuilder: SpritesheetBuilder
 
-    constructor(basePath: string, spritesheetBuilder: SpritesheetBuilder){
-        this.basePath = basePath
-        this.textureCollection = {}
-        this.spritesheetBuilder = spritesheetBuilder
-    }
+        constructor(basePath: string, spritesheetBuilder: SpritesheetBuilder) {
+                this.basePath = basePath
+                this.textureCollection = {}
+                this.spritesheetBuilder = spritesheetBuilder
+        }
 
-    async initTextures(){
-        await this.loadPlayer()
-    }
+        async initTextures() {
+                await this.loadPlayer()
                 await this.loadSlime()
+        }
 
-    private async loadPlayer(){
-        const fileName = 'player.png'
-        this.spritesheetBuilder.setMetaData(this.basePath + fileName, { w: 48, h: 48 })
-        this.spritesheetBuilder.addRow('idle', { x: 0, y: 0 }, 6)
-        this.spritesheetBuilder.addRow('idleRight', { x: 0, y: 48 }, 6)
-        this.spritesheetBuilder.addRow('idleLeft', { x: 0, y: 48 }, 6, true)
-        this.spritesheetBuilder.addRow('idleUp', { x: 0, y: 96 }, 6)
-        this.spritesheetBuilder.addRow('runDown', { x: 0, y: 144 }, 6)
-        this.spritesheetBuilder.addRow('runRight', { x: 0, y: 192 }, 6)
-        this.spritesheetBuilder.addRow('runLeft', { x: 0, y: 192 }, 6, true)
-        this.spritesheetBuilder.addRow('runUp', { x: 0, y: 240 }, 6)
-        this.spritesheetBuilder.addRow('meleeDown', { x: 0, y: 288 }, 4)
-        this.spritesheetBuilder.addRow('meleeRight', { x: 0, y: 336 }, 4)
-        this.spritesheetBuilder.addRow('meleeLeft', { x: 0, y: 336 }, 4, true)
-        this.spritesheetBuilder.addRow('meleeUp', { x: 0, y: 384 }, 4)
-        this.spritesheetBuilder.addRow('knockout', { x: 0, y: 432 }, 3)
-        const assembledTextures = await this.spritesheetBuilder.build()
-        this.textureCollection[fileName] = assembledTextures
-        this.spritesheetBuilder.clear()
-    }
+        private async loadPlayer() {
+                const fileName = 'player.png'
+                this.spritesheetBuilder.setMetaData(fileName, { w: 48, h: 48 })
+                this.spritesheetBuilder.addRow('idle', { x: 0, y: 0 }, 6)
+                this.spritesheetBuilder.addRow('idleRight', { x: 0, y: 48 }, 6)
+                this.spritesheetBuilder.addRow('idleLeft', { x: 0, y: 48 }, 6, true)
+                this.spritesheetBuilder.addRow('idleUp', { x: 0, y: 96 }, 6)
+                this.spritesheetBuilder.addRow('runDown', { x: 0, y: 144 }, 6)
+                this.spritesheetBuilder.addRow('runRight', { x: 0, y: 192 }, 6)
+                this.spritesheetBuilder.addRow('runLeft', { x: 0, y: 192 }, 6, true)
+                this.spritesheetBuilder.addRow('runUp', { x: 0, y: 240 }, 6)
+                this.spritesheetBuilder.addRow('meleeDown', { x: 0, y: 288 }, 4)
+                this.spritesheetBuilder.addRow('meleeRight', { x: 0, y: 336 }, 4)
+                this.spritesheetBuilder.addRow('meleeLeft', { x: 0, y: 336 }, 4, true)
+                this.spritesheetBuilder.addRow('meleeUp', { x: 0, y: 384 }, 4)
+                this.spritesheetBuilder.addRow('knockout', { x: 0, y: 432 }, 3)
+                const assembledTextures = await this.spritesheetBuilder.build()
+                this.textureCollection[fileName] = assembledTextures
+                this.spritesheetBuilder.clear()
+        }
 
-    public getSheet(name: string){
-        return this.textureCollection[name]
-    }
-}                const fileName = 'slime.png'
+        private async loadSlime() {
+                const fileName = 'slime.png'
                 this.spritesheetBuilder.setMetaData(fileName, { w: 32, h: 32 })
                 this.spritesheetBuilder.addRow('idle', { x: 0, y: 0 }, 4)
                 this.spritesheetBuilder.addRow('runDown', { x: 0, y: 32 }, 6)
@@ -58,3 +56,7 @@ export class SpriteLibrary{
                 this.spritesheetBuilder.clear()
         }
 
+        public getSheet(name: string) {
+                return this.textureCollection[name]
+        }
+}
