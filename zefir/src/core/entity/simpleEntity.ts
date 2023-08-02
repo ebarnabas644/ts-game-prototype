3b92840ee8ab716a7a35414eecd2a055abd5c7bd
ebarnabas <macskabal2000@gmail.com>
2023-08-02 19:59:09 +0200

    Prettier

--- zefir/src/core/entity/simpleEntity.ts
+++ zefir/src/core/entity/simpleEntity.ts
@@ -1,65 +1,68 @@
 import { store } from '../gameState'
-import { SpritesheetBuilder } from '../utilities/spriteSheetBuilder';
-import { spriteLibrary } from '../gameMain';
-import { type Sheet } from '../types/sheet';
+import { SpritesheetBuilder } from '../utilities/spriteSheetBuilder'
+import { spriteLibrary } from '../gameMain'
+import { type Sheet } from '../types/sheet'
 import * as PIXI from 'pixi.js'
-
 
 export type SimpleEntityDictionary = SimpleEntity[]
 // Entity class
 export class SimpleEntity {
-  id: number
-  name: string
-  position: { x: number, y: number }
-  sprite: PIXI.AnimatedSprite
-  state: string
-  health: number
-  tags: {[key: string]: any}
-  spriteSheet: {[key: string]: Sheet}
-  flipped: boolean
+        id: number
+        name: string
+        position: { x: number; y: number }
+        sprite: PIXI.AnimatedSprite
+        state: string
+        health: number
+        tags: { [key: string]: any }
+        spriteSheet: { [key: string]: Sheet }
+        flipped: boolean
 
-  constructor(id: number, name: string, position: {x: number, y: number}, spritePath: string, health: number, tags: {[key: string]: any}, state: string) {
-    this.id = id
-    this.name = name
-    this.position = {
-      x: position.x,
-      y: position.y
-    }
-    this.spriteSheet = spriteLibrary.getSheet(spritePath)
-    this.health = health
-    this.state = state
-    this.sprite = new PIXI.AnimatedSprite(this.spriteSheet[this.state].textures)
-    this.sprite.anchor.x = 0.5
-    this.sprite.anchor.y = 0.5
-    this.sprite.animationSpeed = 0.166
-    this.sprite.play()
-    this.sprite.x = position.x
-    this.sprite.y = position.y
-    this.tags = tags
-    this.flipped = false
-  }
+        constructor(
+                id: number,
+                name: string,
+                position: { x: number; y: number },
+                spritePath: string,
+                health: number,
+                tags: { [key: string]: any },
+                state: string
+        ) {
+                this.id = id
+                this.name = name
+                this.position = {
+                        x: position.x,
+                        y: position.y
+                }
+                this.spriteSheet = spriteLibrary.getSheet(spritePath)
+                this.health = health
+                this.state = state
+                this.sprite = new PIXI.AnimatedSprite(this.spriteSheet[this.state].textures)
+                this.sprite.anchor.x = 0.5
+                this.sprite.anchor.y = 0.5
+                this.sprite.animationSpeed = 0.166
+                this.sprite.play()
+                this.sprite.x = position.x
+                this.sprite.y = position.y
+                this.tags = tags
+                this.flipped = false
+        }
 
-  release(){
-    this.sprite.destroy()
-  }
+        release() {
+                this.sprite.destroy()
+        }
 
-  setState(newState: string){
-    if(this.state != newState){
-      this.state = newState
-      this.sprite.stop()
-      console.log(this.spriteSheet[this.state].options.horizontalInvert)
-      if(this.spriteSheet[this.state].options.horizontalInvert){
-        this.sprite.scale.x *= -1
-        this.flipped = true
-      }
-      else if(this.flipped){
-        console.log("reset flip")
-        this.sprite.scale.x = Math.abs(this.sprite.scale.x)
-        this.flipped = false
-      }
-      this.sprite.textures = this.spriteSheet[this.state].textures
-      this.sprite.gotoAndPlay(0)
-  }
-
-}
+        setState(newState: string) {
+                if (this.state != newState) {
+                        this.state = newState
+                        this.sprite.stop()
+                        if (this.spriteSheet[this.state].options.horizontalInvert) {
+                                this.sprite.scale.x *= -1
+                                this.flipped = true
+                        } else if (this.flipped) {
+                                this.sprite.scale.x = Math.abs(this.sprite.scale.x)
+                                this.flipped = false
+                        }
+                        this.sprite.textures = this.spriteSheet[this.state].textures
+                        this.sprite.gotoAndPlay(0)
+                }
+        }
 }
