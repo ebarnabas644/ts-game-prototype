ceb33bd005e4baeed4d482017c37187e2d412ab2
ebarnabas <macskabal2000@gmail.com>
2023-08-02 19:58:54 +0200

    Using input events instead of string and mouse handler

--- zefir/src/core/gameInput.ts
+++ zefir/src/core/gameInput.ts
@@ -1,73 +1,172 @@
 import { emitCustomEvent } from './utilities/customEventEmitter'
+import { gameCamera } from './gameMain'
+import { Vec2 } from '@thi.ng/vectors'
+
+export type InputEvent = {
+        input: string
+        data: { [key: string]: any }
+}
 
 export class InputSystemComponent {
-        private pushedButtons: Set<string>
-        private previous: Set<string>
+        private pushedButtons: InputEvent[]
+        private history: InputEvent[]
         private pressHandler
         private releaseHandler
+        private mousePressHandler
+        private mouseReleaseHandler
+        private mouseMoveHandler
+        private throttleCounter: number
+        private throttleValue: number
         constructor() {
-                this.pushedButtons = new Set()
-                this.previous = new Set()
-                this.pressHandler = (event: any) =>
-                        this.handleButtonPress(event)
-                this.releaseHandler = (event: any) =>
-                        this.handleButtonRelease(event)
+                this.pushedButtons = []
+                this.history = []
+                this.pressHandler = (event: any) => this.handleButtonPress(event)
+                this.releaseHandler = (event: any) => this.handleButtonRelease(event)
+                this.mousePressHandler = (event: any) => this.handleMousePress(event)
+                this.mouseReleaseHandler = (event: any) => this.handleMouseRelease(event)
+                this.mouseMoveHandler = (event: any) => this.handleMouseMove(event)
                 this.enableInput()
+                this.throttleCounter = -1
+                this.throttleValue = 10
         }
 
         public enableInput() {
                 document.addEventListener('keydown', this.pressHandler)
+                document.addEventListener('mousedown', this.mousePressHandler)
+                document.addEventListener('mouseup', this.mouseReleaseHandler)
                 document.addEventListener('keyup', this.releaseHandler)
         }
 
         public disableInput() {
                 document.removeEventListener('keydown', this.pressHandler)
+                document.removeEventListener('mousedown', this.mousePressHandler)
+                document.removeEventListener('mouseup', this.mouseReleaseHandler)
+                document.removeEventListener('mousemove', this.mouseMoveHandler)
                 document.removeEventListener('keyup', this.releaseHandler)
         }
 
         private handleButtonPress(event: any) {
-                this.previous = new Set(this.pushedButtons)
+                this.copyToHistory()
                 if (event.key == 'ArrowLeft' || event.key == 'a') {
-                        this.pushedButtons.add('leftMoveCommand')
+                        const inputEvent: InputEvent = {
+                                input: 'leftMoveCommand',
+                                data: {}
+                        }
+                        this.addInputEvent(inputEvent)
                 }
                 if (event.key == 'ArrowRight' || event.key == 'd') {
-                        this.pushedButtons.add('rightMoveCommand')
+                        const inputEvent: InputEvent = {
+                                input: 'rightMoveCommand',
+                                data: {}
+                        }
+                        this.addInputEvent(inputEvent)
                 }
                 if (event.key == 'ArrowUp' || event.key == 'w') {
-                        this.pushedButtons.add('upMoveCommand')
+                        const inputEvent: InputEvent = {
+                                input: 'upMoveCommand',
+                                data: {}
+                        }
+                        this.addInputEvent(inputEvent)
                 }
                 if (event.key == 'ArrowDown' || event.key == 's') {
-                        this.pushedButtons.add('downMoveCommand')
+                        const inputEvent: InputEvent = {
+                                input: 'downMoveCommand',
+                                data: {}
+                        }
+                        this.addInputEvent(inputEvent)
                 }
 
-                if (!this.previouslyPressedEqualsCurrentlyPressed()) {
+                if (!this.historylyPressedEqualsCurrentlyPressed()) {
                         emitCustomEvent('playerInput', this.pushedButtons)
                 }
         }
 
         private handleButtonRelease(event: any) {
                 if (event.key == 'ArrowLeft' || event.key == 'a') {
-                        this.pushedButtons.delete('leftMoveCommand')
+                        this.removeInputEvent('leftMoveCommand')
                 }
                 if (event.key == 'ArrowRight' || event.key == 'd') {
-                        this.pushedButtons.delete('rightMoveCommand')
+                        this.removeInputEvent('rightMoveCommand')
                 }
                 if (event.key == 'ArrowUp' || event.key == 'w') {
-                        this.pushedButtons.delete('upMoveCommand')
+                        this.removeInputEvent('upMoveCommand')
                 }
                 if (event.key == 'ArrowDown' || event.key == 's') {
-                        this.pushedButtons.delete('downMoveCommand')
+                        this.removeInputEvent('downMoveCommand')
                 }
                 emitCustomEvent('playerInput', this.pushedButtons)
         }
 
-        private previouslyPressedEqualsCurrentlyPressed() {
+        private handleMousePress(event: any) {
+                this.throttleCounter = -1
+                const cursorPosition = new Vec2([event.clientX, event.clientY])
+                const globalCursorPosition = gameCamera.convertLocalPositionToGlobal(cursorPosition)
+                const inputEvent: InputEvent = {
+                        input: 'attack',
+                        data: {
+                                xPos: globalCursorPosition.x,
+                                yPos: globalCursorPosition.y
+                        }
+                }
+                this.addInputEvent(inputEvent)
+                emitCustomEvent('playerInput', this.pushedButtons)
+                document.addEventListener('mousemove', this.mouseMoveHandler)
+        }
+
+        private handleMouseRelease(event: any) {
+                document.removeEventListener('mousemove', this.mouseMoveHandler)
+                this.removeInputEvent('attack')
+                emitCustomEvent('playerInput', this.pushedButtons)
+        }
+
+        private handleMouseMove(event: any) {
+                this.throttleCounter++
+                if (this.throttleCounter % this.throttleValue != 0) return
+                const cursorPosition = new Vec2([event.clientX, event.clientY])
+                const globalCursorPosition = gameCamera.convertLocalPositionToGlobal(cursorPosition)
+                const inputEvent: InputEvent = {
+                        input: 'attack',
+                        data: {
+                                xPos: globalCursorPosition.x,
+                                yPos: globalCursorPosition.y
+                        }
+                }
+                this.addInputEvent(inputEvent)
+                emitCustomEvent('playerInput', this.pushedButtons)
+        }
+
+        private historylyPressedEqualsCurrentlyPressed() {
                 for (const button of this.pushedButtons) {
-                        if (!this.previous.has(button)) {
+                        const result = this.history.find((x) => x.input == button.input)
+                        if (!result) {
                                 return false
                         }
                 }
 
                 return true
+        }
+
+        private addInputEvent(inputToAdd: InputEvent) {
+                const result = this.pushedButtons.find((x) => x.input == inputToAdd.input)
+                if (result) {
+                        result.data = inputToAdd.data
+                        return
+                }
+                this.pushedButtons.push(inputToAdd)
+        }
+
+        private removeInputEvent(input: string) {
+                const result = this.pushedButtons.find((x) => x.input == input)
+                if (!result) return
+                const index = this.pushedButtons.indexOf(result)
+                if (index == -1) return
+                this.pushedButtons.splice(index, 1)
+        }
+
+        private copyToHistory() {
+                this.history = []
+                for (const inputEvent of this.pushedButtons) {
+                        this.history.push(inputEvent)
+                }
         }
 }
