c396c9f506782dbb99da40214f87d53a76a7aa6d
ebarnabas <macskabal2000@gmail.com>
2023-08-03 16:50:53 +0200

    Removing pixi initialization from canvas

--- zefir/src/components/Canvas.vue
+++ zefir/src/components/Canvas.vue
@@ -1,48 +1,14 @@
 <script setup lang="ts">
-import Modal from './Modal.vue';
-import { onMounted, onUnmounted, ref } from 'vue';
-import { initGame, setRenderer } from '@/core/gameMain';
-import * as PIXI from 'pixi.js'
-import { io, Socket } from 'socket.io-client'
-import Chat from './Chat.vue';
-import StatusBar from './StatusBar.vue';
-import Minimap from './Minimap.vue';
-
-const pixiContainer = ref<HTMLElement | null>(null)
-let app: PIXI.Application | null = null
-onMounted(() => {
-  window.addEventListener("resize", resizeCanvas)
-  pixiContainer.value?.focus()
-  app = new PIXI.Application({
-    width: window.innerWidth,
-    height: window.innerHeight,
-    view: pixiContainer.value!.appendChild(document.createElement('canvas')),
-    backgroundColor: 0x5c812f
-  });
-  //globalThis.__PIXI_APP__ = app //for debugging pixi app with browser extension
-  setRenderer(app)
-}
-)
-
-onUnmounted(() => {
-  window.removeEventListener("resize", resizeCanvas)
-})
-
-function resizeCanvas(event: any){
-  app?.renderer.resize(window.innerWidth, window.innerHeight)
-}
-
+import Chat from './Chat.vue'
+import StatusBar from './StatusBar.vue'
+import Minimap from './Minimap.vue'
 </script>
 
 <template>
-  <div ref="pixiContainer"></div>
-  <Chat :defaultX="0.05" :defaultY="0.7"></Chat>
-  <StatusBar :defaultX="0.05" :defaultY="0.05"></StatusBar>
-  <Minimap :defaultX="0.90" :defaultY="0.05"></Minimap>
+        <div id="pixiContainer"></div>
+        <Chat :defaultX="0.05" :defaultY="0.7"></Chat>
+        <StatusBar :defaultX="0.05" :defaultY="0.05"></StatusBar>
+        <Minimap :defaultX="0.9" :defaultY="0.05"></Minimap>
 </template>
 
-<style scoped>
-canvas {
-      border: 1px solid white;
-    }
-</style>
+<style scoped></style>
